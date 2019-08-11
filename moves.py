3daf650b4c6fd06a594e2cd291da69e166559ab1
Brady Richardson <47166439+bradyrichardson@users.noreply.github.com>
2019-08-11 18:52:44 -0400

    Delete moves.py

--- moves.py
+++
@@ -1,9 +0,0 @@
-class Moves:
-    def __init__(self, visited, visitedArray, possibleMoves):
-        self.visited = False
-        self.visitedArray = []
-        self.possibleMoves = []
-
-    def checkCanMove(self):
-        for moves in self.possibleMoves:
-            print(moves)
